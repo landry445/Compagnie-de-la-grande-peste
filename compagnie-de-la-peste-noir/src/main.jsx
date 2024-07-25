fda9ff254ecb77a4129a97bc8f4a71a9c5aead14
Florent Moulin <florent@MacBook-de-Florent.local>
2024-07-25 15:21:02 +0200

    API connected

--- compagnie-de-la-peste-noir/src/main.jsx
+++ compagnie-de-la-peste-noir/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
 
 const router = createBrowserRouter([
   {
@@ -12,13 +13,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-=======
-    
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> dev

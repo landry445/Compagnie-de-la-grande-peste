38284d507f7035edc0e4d11431e590cd6e6fe8e9
Alix <alix.champagnol@gmail.com>
2024-07-25 20:56:03 +0200

    ajout modif pages Contact

--- compagnie-de-la-peste-noir/src/main.jsx
+++ compagnie-de-la-peste-noir/src/main.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";  
+import Contact from "./Pages/Contact";
+import Faq from "./Pages/Faq";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/contact", 
+    element: <Contact />, 
+  },
+  {
+    path: "/faq", 
+    element: <Faq />, 
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
